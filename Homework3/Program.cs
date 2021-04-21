6750886acc2f0bde1d8833b47b810cf922c1b48f
Danila Zhogol <danzhog@gmail.com>
2021-04-21 14:10:02 +0300

    Added new data member "secretAnswer"

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -8,23 +8,26 @@ namespace QAAutomationFirstProg
         {
             private string loginName;
             private string password;
-            public Credentials(string loginName, string password)
+            private string secretAnswer;
+            public Credentials(string loginName, string password, string secretAnswer)
             {
                 this.loginName = loginName;
                 this.password = password;
+                this.secretAnswer = secretAnswer;
             }
             public bool IsEquals(Credentials creds)
             {
-                return (loginName == creds.loginName && password == creds.password);
+                return (loginName == creds.loginName && password == creds.password && secretAnswer == creds.secretAnswer);
             }
         }
         // Entry point
         static void Main(string[] args)
         {
-            Credentials cred = new Credentials("name2", "password2");
+            Credentials cred = new Credentials("name2", "password2", "secretAnswer2");
             if (CheckAuthentification(cred) == true)
             {
                 Login();
+                Console.WriteLine("Successful!");
             }
             else
             {
@@ -51,9 +54,9 @@ namespace QAAutomationFirstProg
         {
             // Data base
             Credentials[] dataBaseCreds = new Credentials[3];
-            dataBaseCreds[0] = new Credentials("name1", "password1");
-            dataBaseCreds[1] = new Credentials("name2", "password2");
-            dataBaseCreds[2] = new Credentials("name3", "password3");
+            dataBaseCreds[0] = new Credentials("name1", "password1", "secretAnswer1");
+            dataBaseCreds[1] = new Credentials("name2", "password2", "secretAnswer2");
+            dataBaseCreds[2] = new Credentials("name3", "password3", "secretAnswer3");
             return dataBaseCreds;
         }
     }

1da18e9de3ce6f83277dfa1203c42a0016bf5338
Danila Zhogol <danzhog@gmail.com>
2021-10-07 13:03:34 +0300

    initial commit

--- Homework4/Car.cs
+++ Homework4/Car.cs
@@ -16,7 +16,7 @@ namespace Homework4
 
         public override string GetFullInfo() //вывод всей информации о ТС
         {
-            return "Bus " + ModelName + " with an Engine Capacity of " + EngineCapacity + " liters, Transmission Type of "
+            return "Car " + ModelName + " with an Engine Capacity of " + EngineCapacity + " liters, Transmission Type of "
                 + TransmissionType + ", Maximum Speed of " + MaxSpeed + "km/h and Car Body Style of "
                 + CarBodyStyle;
         }

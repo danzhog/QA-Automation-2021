fe383dd6bd3fc5b53319eb2158637898efcd47ad
Danila Zhogol <danzhog@gmail.com>
2022-02-19 14:58:11 +0300

    Added class "VideoStore" and method "TakeCassette" to class Person

---
+++ VideoStore/VideoStore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VideoStore
+{
+    public class VideoStore
+    {
+        public List<Cassette> CassettesList { get; set; }
+        public VideoStore(List<Cassette> cassettesList)
+        {
+            CassettesList = cassettesList;
+        }
+
+        public void AddCassetteToList(Cassette cassette)
+        {
+            CassettesList.Add(cassette);
+        }
+    }
+}
